d957ee33c85ed8753f388b57cdc8262c5ff11c78
haploide <haploide1998@hotmail.com>
2017-07-08 23:46:01 -0300

    configurado angular, jquery, moment y creados los archivos para trabajar en nuestros scripts

--- Producto/02 Modulo Web - Codigo Fuente/GeoTurnos/WebSite/App_Start/BundleConfig.cs
+++ Producto/02 Modulo Web - Codigo Fuente/GeoTurnos/WebSite/App_Start/BundleConfig.cs
@@ -8,8 +8,15 @@ namespace WebSite
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/angular.min.js",
+                        "~/Scripts/angular-route.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/moment").Include(
+                        "~/Scripts/moment.min.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-3.1.1.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
