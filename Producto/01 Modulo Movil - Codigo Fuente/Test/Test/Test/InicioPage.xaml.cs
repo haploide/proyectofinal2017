ceea92ea736efb49ce616b19cecd959d4957033b
Diego Moreno <diego149@gmail.com>
2017-07-19 19:15:17 -0300

    Test de Emulador

---
+++ Producto/01 Modulo Movil - Codigo Fuente/Test/Test/Test/InicioPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Test
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InicioPage : ContentPage
+    {
+        public InicioPage()
+        {
+            InitializeComponent();
+        }
+
+        public void btnLoginClicked (Object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(usuarioEntry.Text))
+            {
+                usuarioEntry.Placeholder = "Debe ingresar su usuario";
+                usuarioEntry.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(passEntry.Text))
+            {
+                passEntry.Placeholder = "Debe ingresar su Password";
+                passEntry.Focus();
+                return;
+            }
+        }
+    }
+}
