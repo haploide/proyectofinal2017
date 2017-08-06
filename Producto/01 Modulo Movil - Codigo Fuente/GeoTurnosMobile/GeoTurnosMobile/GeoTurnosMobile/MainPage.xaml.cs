996bf8b81e4554748dbe4caf0fdc22976382a512
Diego Moreno <diego149@gmail.com>
2017-08-06 10:42:50 -0300

    GeoTurnos Mobile

---
+++ Producto/01 Modulo Movil - Codigo Fuente/GeoTurnosMobile/GeoTurnosMobile/GeoTurnosMobile/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GeoTurnosMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void btnLoginClicked(Object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(usuarioEntry.Text))
+            {
+                usuarioEntry.Placeholder = "Debe ingresar su usuario";
+                usuarioEntry.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(passEntry.Text))
+            {
+                passEntry.Placeholder = "Debe ingresar su Password";
+                passEntry.Focus();
+                return;
+            }
+
+
+            ///////////////////////////////////////////
+            //Si se loguea se redirecciona a 
+            this.Navigation.PushModalAsync(new MenuPrincipal());
+            //////////////////////////////////////////
+        }
+
+        public void btnOlvidoPassClicked(Object sender, EventArgs e)
+        {
+            this.Navigation.PushModalAsync(new OlvidoPassword());
+        }
+
+        public void btnRegistrarseClicked(Object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("http://www.google.com"));
+        }
+    }
+}
---
+++ Producto/01 Modulo Movil - Codigo Fuente/GeoTurnosMobile/GeoTurnosMobile/GeoTurnosMobile/MenuPrincipal.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GeoTurnosMobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPrincipal : ContentPage
+    {
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        public void btnNuevoTurnoClicked(Object sender, EventArgs e)
+        {
+
+        }
+
+        public void btnMisTurnosClicked(Object sender, EventArgs e)
+        {
+
+        }
+    }
+}
