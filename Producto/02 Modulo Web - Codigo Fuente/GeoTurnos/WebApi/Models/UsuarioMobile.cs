3b116d622a93f391fac7990569f4898ad9b54b2c
haploide <haploide1998@hotmail.com>
2017-08-07 22:39:18 -0300

    librerias restfull para mobile

---
+++ Producto/02 Modulo Web - Codigo Fuente/GeoTurnos/WebApi/Models/UsuarioMobile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class UsuarioMobile
+    {
+        //ToDO: Completar a medida que se necesiten mas datos
+
+        public string usuario { get; set; }
+        public string nombreEstado { get; set; }
+        public string rol { get; set; }
+
+    }
+}
