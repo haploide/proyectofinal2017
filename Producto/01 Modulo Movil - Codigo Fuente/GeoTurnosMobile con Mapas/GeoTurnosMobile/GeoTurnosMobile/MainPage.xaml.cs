474093ddd334f076ab67f9155f8af5c146f51dc3
diego149 <diego149@gmail.com>
2017-09-23 12:13:31 -0300

    App con mapas integrados

---
+++ Producto/01 Modulo Movil - Codigo Fuente/GeoTurnosMobile con Mapas/GeoTurnosMobile/GeoTurnosMobile/MainPage.xaml.cs
@@ -0,0 +1,111 @@
+﻿using Org.BouncyCastle.Crypto.Digests;
+using System;
+using System.Net.Http;
+using System.Text;
+using Xamarin.Forms;
+
+namespace GeoTurnosMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void btnLoginClicked(Object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(usuarioEntry.Text))
+            {
+                await DisplayAlert("Error", "Debe ingresar su usuario", "Aceptar");
+                usuarioEntry.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(passEntry.Text))
+            {
+                await DisplayAlert("Error", "Debe ingresar su Password", "Aceptar");
+                passEntry.Focus();
+                return;
+            }
+            estaEsperando(true);
+            
+            try
+            {
+                HttpClient cliente = new HttpClient();
+                cliente.BaseAddress = new Uri("http://192.168.1.102:8081");
+                string url = string.Format("/api/login?usuario={0}&password={1}", usuarioEntry.Text, Hashear(passEntry.Text).ToUpper());
+                var response = await cliente.GetAsync(url);
+
+                estaEsperando(false);
+
+                if (response.StatusCode == System.Net.HttpStatusCode.InternalServerError)
+                {
+                    await DisplayAlert("Error", "Error al procesar la solicitud. Por favor inténtalo nuevamente", "Aceptar");
+                    passEntry.Focus();
+                    return;
+                }
+
+                if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                {
+                    await DisplayAlert("Error", "Usuario o Password Incorrecto", "Aceptar");
+                    passEntry.Focus();
+                    return;
+                }
+
+                ///////////////////////////////////////////
+                //Si se loguea se redirecciona a 
+                await Navigation.PushModalAsync(new MenuPrincipal());
+                //////////////////////////////////////////
+            }
+            catch (Exception ex)
+            {
+                estaEsperando(false);
+
+                await DisplayAlert("Error", ex.Message, "Aceptar");
+                
+            }
+        }
+
+        public void btnOlvidoPassClicked(Object sender, EventArgs e)
+        {
+            this.Navigation.PushModalAsync(new OlvidoPassword());
+        }
+
+        public void btnRegistrarseClicked(Object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("http://192.168.1.102:8080"));
+        }
+        private void estaEsperando(bool esperar)
+        {
+            awaitActivityIndicator.IsRunning = esperar;
+            btnLogin.IsEnabled = !esperar;
+            usuarioEntry.IsEnabled = !esperar;
+            passEntry.IsEnabled = !esperar;
+            btnOlvidoPass.IsEnabled = !esperar;
+            btnRegistrarse.IsEnabled = !esperar;
+
+        }
+        private string Hashear(string textoPlano)
+        {
+            var encodeData = Encoding.UTF8.GetBytes(textoPlano);
+
+            Sha256Digest hash = new Sha256Digest();
+
+            hash.BlockUpdate(encodeData, 0, encodeData.Length);
+
+            byte[] compArr = new byte[hash.GetDigestSize()];
+
+            hash.DoFinal(compArr, 0);
+
+            StringBuilder sb = new StringBuilder();
+            
+            for (int i = 0; i < compArr.Length; i++)
+            {
+                sb.Append(compArr[i].ToString("x2"));
+            }
+                      
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ Producto/01 Modulo Movil - Codigo Fuente/GeoTurnosMobile con Mapas/GeoTurnosMobile/GeoTurnosMobile/MapPage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms;
+
+namespace GeoTurnosMobile
+{
+    class MapPage : ContentPage
+    {
+        public Map myMap;
+        public MapPage()
+        {
+            var settings = new ToolbarItem
+            {
+
+            };
+
+            this.ToolbarItems.Add(settings);
+
+            myMap = new Map(
+            MapSpan.FromCenterAndRadius(new Position(-31.42, -64.19), Distance.FromMiles(0.3)))
+            {
+
+                IsShowingUser = true,
+                HeightRequest = 200,
+                WidthRequest = 320,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                MapType = MapType.Street
+            };
+
+
+            //var slider = new Slider(1, 18, 1);
+            //slider.ValueChanged += (sender, e) => {
+            //    var zoomLevel = e.NewValue; // between 1 and 18
+            //    var latlongdegrees = 360 / (Math.Pow(2, zoomLevel));
+            //    myMap.MoveToRegion(new MapSpan(myMap.VisibleRegion.Center, latlongdegrees, latlongdegrees));
+            //};
+
+
+
+            var stack = new StackLayout { Spacing = 0 };
+            stack.Children.Add(myMap);
+            Content = stack;
+        }
+    }
+}
