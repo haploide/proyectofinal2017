89b4c80b1dd715bdf88cf8b5a253885b609bfd01
freddykpo <federico_leiva@hotmail.com>
2017-10-28 18:47:16 -0300

    Actualizado

---
+++ Producto/02 Modulo Web - Codigo Fuente/GeoTurnos/WebApi/Controllers/VistaTurnosAgendaCliente.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [EnableCors(origins: "http://localhost:6907", headers: "*", methods: "*")]
+    public class VistaTurnosAgendaCliente : ApiController
+    {
+        private GeoTurnosEntities _db = new GeoTurnosEntities();
+
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
