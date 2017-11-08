f1e7ed97d4ef23183d4f129f1ac68e3ea4ee6cd6
diego149 <diego149@gmail.com>
2017-11-08 02:48:16 -0300

    GeoTurnos Movil - Pantalla Principal con datos del usuario

--- Producto/01 Modulo Movil - Codigo Fuente/GeoTurnos-FinalVersion/app/src/main/java/com/example/ld/mapa/PantallaPrincipal.java
+++ Producto/01 Modulo Movil - Codigo Fuente/GeoTurnos-FinalVersion/app/src/main/java/com/example/ld/mapa/PantallaPrincipal.java
@@ -1,6 +1,8 @@
 package com.example.ld.mapa;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
@@ -11,8 +13,16 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 /**
@@ -27,6 +37,8 @@ public class PantallaPrincipal extends ActionBarActivity {
     private CharSequence activityTitle;
     private CharSequence itemTitle;
     private String[] tagTitles;
+    private ImageView avatar;
+    private TextView bienvenido;
 
 
     @Override
@@ -38,6 +50,8 @@ public class PantallaPrincipal extends ActionBarActivity {
         tagTitles = getResources().getStringArray(R.array.Tags);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawerList = (ListView) findViewById(R.id.left_drawer);
+        avatar=(ImageView) findViewById(R.id.avatar);
+        bienvenido=(TextView)findViewById(R.id.lnkUsuario);
 
         // Setear una sombra sobre el contenido principal cuando el drawer se despliegue
         drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
@@ -72,6 +86,31 @@ public class PantallaPrincipal extends ActionBarActivity {
 
         if (savedInstanceState == null) {
             selectItem(0);
+        }
+
+
+        Blob imagen=null;
+        String valor = getIntent().getExtras().getString("idUsuario");
+        String consulta="";
+        ResultSet resulset;
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            String cn = "jdbc:jtds:sqlserver://geoturnos.com:49172;databaseName=GeoTurnos;user=geoturnos;password=Al1.B4b4-";
+            Connection connection = DriverManager.getConnection(cn);
+            Statement statement = connection.createStatement();
+            consulta = "SELECT foto, usuario FROM Cliente T1 JOIN Usuario T2 ON T1.idUsuario=T2.idUsuario WHERE T1.idUsuario='"+valor.toString()+"'";
+            resulset = statement.executeQuery(consulta);
+            for (int i=0;resulset.next();i++){
+                byte[] byteArray = resulset.getBytes("foto");
+                Bitmap bm = BitmapFactory.decodeByteArray(byteArray, 0 ,byteArray.length);
+                avatar.setImageBitmap(bm);
+                bienvenido.setText(bienvenido.getText().toString() + resulset.getString("usuario"));
+
+            }
+        } catch ( ClassNotFoundException e){
+
+        } catch (SQLException e){
+
         }
 }
 
